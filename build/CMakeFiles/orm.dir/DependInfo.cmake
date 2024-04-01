2bfa7fed0e01b128fc7d517911d0d0ce22453f75
trranspar <2280178143@qq.com>
2024-04-01 11:04:05 +0800

    Initial commit

---
+++ build/CMakeFiles/orm.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/workspace/sylar/sylar/orm/column.cc" "/root/workspace/sylar/build/CMakeFiles/orm.dir/sylar/orm/column.cc.o"
+  "/root/workspace/sylar/sylar/orm/index.cc" "/root/workspace/sylar/build/CMakeFiles/orm.dir/sylar/orm/index.cc.o"
+  "/root/workspace/sylar/sylar/orm/orm.cc" "/root/workspace/sylar/build/CMakeFiles/orm.dir/sylar/orm/orm.cc.o"
+  "/root/workspace/sylar/sylar/orm/table.cc" "/root/workspace/sylar/build/CMakeFiles/orm.dir/sylar/orm/table.cc.o"
+  "/root/workspace/sylar/sylar/orm/util.cc" "/root/workspace/sylar/build/CMakeFiles/orm.dir/sylar/orm/util.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DBUG_OFF"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "/usr/include/mysql"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/workspace/sylar/build/CMakeFiles/sylar.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/orm.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orm.dir/sylar/orm/table.cc.o"
+  "CMakeFiles/orm.dir/sylar/orm/column.cc.o"
+  "CMakeFiles/orm.dir/sylar/orm/index.cc.o"
+  "CMakeFiles/orm.dir/sylar/orm/orm.cc.o"
+  "CMakeFiles/orm.dir/sylar/orm/util.cc.o"
+  "../bin/orm.pdb"
+  "../bin/orm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
